71dee918f29a99701cb109a814639ab067db67af
HarishBaki <harish.iiitn@gmail.com>
2024-10-03 14:38:36 -0400

    Refactor download script for ERA5 data

---
+++ run_all_download_ERA5_hbakialbany.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# conda activate cdsapi
+main_dir=$(pwd)
+cdsapi_file='/home/harish/.cdsapirc_hbakialbany'
+target_dir='/data/harish/Estimation-of-lidar-wind-speed-profiles-from-ERA5-inputs-using-TabNet/data'
+year=2020
+mkdir -p $target_dir"/"$year
+cd $target_dir"/"$year
+    for month in $(seq 1 1 12)
+    do
+        # --- downloading the PRES_UVT file --- #
+        if ls "PRES_UVT_"$year"_"$month".nc" 1>/dev/null 2>&1;
+        then
+            x_files=($(ls "PRES_UVT_"$year"_"$month".nc"))
+            echo "File exists:"$(ls -l ${x_files[*]})
+        else
+            echo "PRES_UVT File is missing"
+            python $main_dir/download_ERA5_PRES_UVT.py $cdsapi_file $year $month
+        fi
+
+        # --- downloading the SFC file --- #
+        if ls "SFC_"$year"_"$month".nc" 1>/dev/null 2>&1;
+        then
+            x_files=($(ls "SFC_"$year"_"$month".nc"))
+            echo "File exists:"$(ls -l ${x_files[*]})
+        else
+            echo "SFC File is missing"
+            python $main_dir/download_ERA5_SFC.py $cdsapi_file $year $month
+        fi
+    done
+cd $main_dir
+
+echo "All files downloaded"
---
+++ run_all_download_ERA5_sandhya.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# conda activate cdsapi
+main_dir=$(pwd)
+
+target_dir='/data/harish/WRF-PBL-Scheme-Evaluation-with-Chebyshev-Approximation-and-Lidar-Wind-Profiles/Input_DATA'
+year=2023
+mkdir -p $target_dir"/"$year
+cd $target_dir"/"$year
+    for month in $(seq 10 1 12)
+    do
+        if ls "PRES_SC_"$year"_"$month".grb" \
+            && ls "PRES_UVT_"$year"_"$month".grb" \
+            && ls "SFC_"$year"_"$month".grb" 1>/dev/null 2>&1;
+        then
+            x_files=($(ls "PRES_SC_"$year"_"$month".grb" "PRES_UVT_"$year"_"$month".grb" "SFC_"$year"_"$month".grb"))
+            echo "All files exist:"$(ls -l ${x_files[*]})
+        else
+            echo "One or more files are missing" 
+            python $main_dir/download_ERA5.py '/home/harish/.cdsapirc_sandhya' $year $month &
+        fi
+    done
+    wait
+cd $main_dir
+
+echo "All files downloaded"
