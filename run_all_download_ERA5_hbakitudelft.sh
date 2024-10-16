77d95bbb280a7df7e8a5a86afd789fc474fb5c18
HarishBaki <harish.iiitn@gmail.com>
2024-10-15 21:39:07 -0400

    Refactor download script to iterate over multiple years and months

--- run_all_download_ERA5_hbakitudelft.sh
+++ run_all_download_ERA5_hbakitudelft.sh
@@ -3,36 +3,37 @@
 main_dir=$(pwd)
 cdsapi_file='/home/harish/.cdsapirc_hbakitudelft'
 target_dir='/data/harish/Estimation-of-lidar-wind-speed-profiles-from-ERA5-inputs-using-TabNet/data'
-year=2021
-mkdir -p $target_dir"/"$year
-cd $target_dir"/"$year
-    for month in $(seq 1 1 12)
-    do
-        # change month to 2 digits
-        if [ $month -lt 10 ]
-        then
-            month="0"$month
-        fi
-        # --- downloading the PRES_UVT file --- #
-        if ls "PRES_UVT_"$year"_"$month".nc" 1>/dev/null 2>&1;
-        then
-            x_files=($(ls "PRES_UVT_"$year"_"$month".nc"))
-            echo "File exists:"$(ls -l ${x_files[*]})
-        else
-            echo "PRES_UVT File is missing"
-            python $main_dir/download_ERA5_PRES_UVT.py $cdsapi_file $year $month
-        fi
+for year in $(seq 2012 4 2017)
+do
+    mkdir -p $target_dir"/"$year
+    cd $target_dir"/"$year
+        for month in $(seq 1 1 12)
+        do
+            # change month to 2 digits
+            if [ $month -lt 10 ]
+            then
+                month="0"$month
+            fi
+            # --- downloading the PRES_UVT file --- #
+            if ls "PRES_UVT_"$year"_"$month".nc" 1>/dev/null 2>&1;
+            then
+                x_files=($(ls "PRES_UVT_"$year"_"$month".nc"))
+                echo "File exists:"$(ls -l ${x_files[*]})
+            else
+                echo "PRES_UVT File is missing"
+                python $main_dir/download_ERA5_PRES_UVT.py $cdsapi_file $year $month
+            fi
 
-        # --- downloading the SFC file --- #
-        if ls "SFC_"$year"_"$month".nc" 1>/dev/null 2>&1;
-        then
-            x_files=($(ls "SFC_"$year"_"$month".nc"))
-            echo "File exists:"$(ls -l ${x_files[*]})
-        else
-            echo "SFC File is missing"
-            python $main_dir/download_ERA5_SFC.py $cdsapi_file $year $month
-        fi
-    done
-cd $main_dir
-
+            # --- downloading the SFC file --- #
+            if ls "SFC_"$year"_"$month".nc" 1>/dev/null 2>&1;
+            then
+                x_files=($(ls "SFC_"$year"_"$month".nc"))
+                echo "File exists:"$(ls -l ${x_files[*]})
+            else
+                echo "SFC File is missing"
+                python $main_dir/download_ERA5_SFC.py $cdsapi_file $year $month
+            fi
+        done
+    cd $main_dir
+done
 echo "All files downloaded"
