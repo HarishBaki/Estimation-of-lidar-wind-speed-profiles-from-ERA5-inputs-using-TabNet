7148eb8bdc49fe18c33a5a8213ad897edd4fc48e
HarishBaki <harish.iiitn@gmail.com>
2024-10-29 23:11:26 -0400

    Add EMD function for Wasserstein distance calculation in plotters.py

--- plotters.py
+++ plotters.py
@@ -4,6 +4,8 @@ import seaborn as sns
 import joblib
 import pickle
 
+
+from scipy import stats
 from sklearn import preprocessing
 from sklearn.metrics import mean_absolute_error as mae
 from sklearn.metrics import mean_squared_error as mse
@@ -11,6 +13,13 @@ from sklearn.metrics import mean_absolute_percentage_error as mape
 from sklearn.metrics import r2_score as R2
 
 from libraries import *
+
+def EMD(df1,df2):
+    bins = np.arange(0,25.1,0.1)
+    hist1 = np.histogram(df1,bins,density=True)[0]
+    hist2 = np.histogram(df2,bins,density=True)[0]
+    #return stats.wasserstein_distance(bins[:-1],bins[:-1],u_weights=hist1,v_weights=hist2)
+    return stats.wasserstein_distance(df1,df2)
 
 def hexbin_plotter(fig,gs,Y,pred,title,text_arg=None,xlabel=None,ylabel=None):
     '''
@@ -28,6 +37,7 @@ def hexbin_plotter(fig,gs,Y,pred,title,text_arg=None,xlabel=None,ylabel=None):
     errRMSE   = np.sqrt(mse(Y,pred))
     errMAPE   = mape(Y,pred)
     errR2     = R2(Y,pred)
+    #errEMD      = EMD(Y,pred)
 
     ax_hexbin = fig.add_subplot(gs)
     hb = ax_hexbin.hexbin(np.squeeze(Y), np.squeeze(pred), gridsize=100, bins='log', cmap='inferno')
